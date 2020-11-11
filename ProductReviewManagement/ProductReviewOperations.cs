a2638c5e283babc57ae418522d9bc3747d8bb947
Satantra Tewari <satantra1508@gmail.com>
2020-11-11 19:06:23 +0530

    [satantra] added method to find products with ids 1,4,9 and rating greater than 3

--- ProductReviewManagement/ProductReviewOperations.cs
+++ ProductReviewManagement/ProductReviewOperations.cs
@@ -13,5 +13,11 @@ namespace ProductReviewManagement
             foreach(var list in recordedData)
                 Console.WriteLine("ProductID: " + list.ProductID + "\nUserId: " + list.UserID + "\nRating: " + list.Rating + "\nReview: " + list.Review + "\nIsLike: " + list.isLike);
         }
+        public void GetProductsRatingGreaterThanThreeAndProductID149(List<ProductReview> productReview)
+        {
+            var recordedData = from products in productReview where (products.ProductID == 1 || products.ProductID == 4 || products.ProductID == 9) && products.Rating > 3 select products;
+            foreach(var list in recordedData)
+                Console.WriteLine("ProductID: " + list.ProductID + "\nUserId: " + list.UserID + "\nRating: " + list.Rating + "\nReview: " + list.Review + "\nIsLike: " + list.isLike);
+        }
     }
 }
