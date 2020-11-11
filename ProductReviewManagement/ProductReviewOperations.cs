6bd07b7121c396c5eae9c270b4ff72b6023734ec
Satantra Tewari <satantra1508@gmail.com>
2020-11-11 18:57:15 +0530

    [satantra] added method to retrieve top 3 best rated products

---
+++ ProductReviewManagement/ProductReviewOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    public class ProductReviewOperations
+    {
+        public void GetTopBestRatedProducts(List<ProductReview> productReview)
+        {
+            var recordedData = (from products in productReview orderby products.Rating descending select products).Take(3);
+            foreach(var list in recordedData)
+                Console.WriteLine("ProductID: " + list.ProductID + "\nUserId: " + list.UserID + "\nRating: " + list.Rating + "\nReview: " + list.Review + "\nIsLike: " + list.isLike);
+        }
+    }
+}
